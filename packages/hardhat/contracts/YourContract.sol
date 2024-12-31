e85bcedb933a04624b3b5372c1948768deca08a9
dradedam <130477584+dradedam@users.noreply.github.com>
2024-12-31 15:34:24 +0500

    Add files via upload

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,78 +1,89 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
-
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
 contract YourContract {
-    // State Variables
-    address public immutable owner;
-    string public greeting = "Building Unstoppable Apps!!!";
-    bool public premium = false;
-    uint256 public totalCounter = 0;
-    mapping(address => uint) public userGreetingCounter;
-
-    // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event GreetingChange(address indexed greetingSetter, string newGreeting, bool premium, uint256 value);
-
-    // Constructor: Called once on contract deployment
-    // Check packages/hardhat/deploy/00_deploy_your_contract.ts
-    constructor(address _owner) {
-        owner = _owner;
+    // Структура для хранения данных о дипломе
+    struct Diploma {
+        string institution;
+        string degree;
+        string major;
+        uint256 graduationYear;
     }
 
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
+    mapping(address => Diploma[]) private diplomas;
+
+    event DiplomaAdded(
+        address indexed owner,
+        string institution,
+        string degree,
+        string major,
+        uint256 graduationYear
+    );
+
+    /**
+     * @notice Добавляет диплом в список владельца
+     * @param _institution Учебное заведение
+     * @param _degree Степень
+     * @param _major Специальность
+     * @param _graduationYear Год выпуска
+     */
+    function addDiploma(
+        string calldata _institution,
+        string calldata _degree,
+        string calldata _major,
+        uint256 _graduationYear
+    ) external {
+        diplomas[msg.sender].push(Diploma({
+            institution: _institution,
+            degree: _degree,
+            major: _major,
+            graduationYear: _graduationYear
+        }));
+
+        emit DiplomaAdded(msg.sender, _institution, _degree, _major, _graduationYear);
     }
 
     /**
-     * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-     *
-     * @param _newGreeting (string memory) - new greeting to save on the contract
+     * @notice Возвращает все дипломы указанного пользователя
+     * @param _owner Адрес пользователя
+     * @return Массив структур Diploma
      */
-    function setGreeting(string memory _newGreeting) public payable {
-        // Print data to the hardhat chain console. Remove when deploying to a live network.
-        console.log("Setting new greeting '%s' from %s", _newGreeting, msg.sender);
+    function getDiplomas(address _owner) external view returns (Diploma[] memory) {
+        return diplomas[_owner];
+    }
 
-        // Change state variables
-        greeting = _newGreeting;
-        totalCounter += 1;
-        userGreetingCounter[msg.sender] += 1;
+    /**
+     * @notice Проверяет наличие диплома по ключевым данным
+     * @param _owner Адрес владельца диплома
+     * @param _institution Учебное заведение
+     * @param _degree Степень
+     * @param _major Специальность
+     * @param _graduationYear Год выпуска
+     * @return True, если диплом найден, иначе False
+     */
+    function hasDiploma(
+        address _owner,
+        string calldata _institution,
+        string calldata _degree,
+        string calldata _major,
+        uint256 _graduationYear
+    ) external view returns (bool) {
+        Diploma[] memory userDiplomas = diplomas[_owner];
+        bytes32 institutionHash = keccak256(abi.encodePacked(_institution));
+        bytes32 degreeHash = keccak256(abi.encodePacked(_degree));
+        bytes32 majorHash = keccak256(abi.encodePacked(_major));
 
-        // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-        if (msg.value > 0) {
-            premium = true;
-        } else {
-            premium = false;
+        for (uint256 i = 0; i < userDiplomas.length; i++) {
+            Diploma memory diploma = userDiplomas[i];
+            if (
+                keccak256(abi.encodePacked(diploma.institution)) == institutionHash &&
+                keccak256(abi.encodePacked(diploma.degree)) == degreeHash &&
+                keccak256(abi.encodePacked(diploma.major)) == majorHash &&
+                diploma.graduationYear == _graduationYear
+            ) {
+                return true;
+            }
         }
-
-        // emit: keyword used to trigger an event
-        emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+        return false;
     }
-
-    /**
-     * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
-     */
-    function withdraw() public isOwner {
-        (bool success, ) = owner.call{ value: address(this).balance }("");
-        require(success, "Failed to send Ether");
-    }
-
-    /**
-     * Function that allows the contract to receive ETH
-     */
-    receive() external payable {}
 }
